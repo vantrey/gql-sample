289754c976ef7d011bc6070c2349e244b1ddf6f2
Tolkachev <vantrey@yandex.ru>
2023-07-21 21:45:53 +0300

    fix http

--- apps/gql-payments/src/main.ts
+++ apps/gql-payments/src/main.ts
@@ -2,8 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { PaymentsModule } from './payments.module';
 
 async function bootstrap() {
-  console.log('service-payment - 3002');
   const app = await NestFactory.create(PaymentsModule);
   await app.listen(3002);
+  console.log('service-payment - 3002');
 }
 bootstrap();
--- libs/http-users/src/http-users.module.ts
+++ libs/http-users/src/http-users.module.ts
@@ -3,7 +3,7 @@ import { HttpUsersService } from './http-users.service';
 import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [HttpModule.register({ baseURL: 'http://localhost:3004/users' })],
+  imports: [HttpModule.register({ baseURL: 'http://localhost:3001/users' })],
   providers: [HttpUsersService],
   exports: [HttpUsersService],
 })
